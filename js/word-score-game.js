5151f747a1f27921afa2fd6c860210ff03cf866a
jeilazhang <44077573+jeilazhang@users.noreply.github.com>
2018-10-13 11:57:51 +0800

    Add files via upload

---
+++ js/word-score-game.js
@@ -0,0 +1,314 @@
+//词库（字母+分数）
+var BAG_OF_LETTERS = [
+		new Letter('_', 2, 0),
+		new Letter('_', 2, 0),
+		new Letter('A', 9, 1),
+		new Letter('A', 9, 1),
+		new Letter('A', 9, 1),
+		new Letter('A', 9, 1),
+		new Letter('A', 9, 1),
+		new Letter('A', 9, 1),
+		new Letter('A', 9, 1),
+		new Letter('A', 9, 1),
+		new Letter('A', 9, 1),
+		new Letter('B', 2, 3),
+		new Letter('B', 2, 3),
+		new Letter('C', 2, 3),
+		new Letter('C', 2, 3),
+		new Letter('D', 4, 2),
+		new Letter('D', 4, 2),
+		new Letter('D', 4, 2),
+		new Letter('D', 4, 2),
+		new Letter('E', 12, 1),
+		new Letter('E', 12, 1),
+		new Letter('E', 12, 1),
+		new Letter('E', 12, 1),
+		new Letter('E', 12, 1),
+		new Letter('E', 12, 1),
+		new Letter('E', 12, 1),
+		new Letter('E', 12, 1),
+		new Letter('E', 12, 1),
+		new Letter('E', 12, 1),
+		new Letter('E', 12, 1),
+		new Letter('E', 12, 1),
+		new Letter('F', 2, 4),
+		new Letter('F', 2, 4),
+		new Letter('G', 3, 2),
+		new Letter('G', 3, 2),
+		new Letter('G', 3, 2),
+		new Letter('H', 2, 4),
+		new Letter('H', 2, 4),
+		new Letter('I', 9, 1),
+		new Letter('I', 9, 1),
+		new Letter('I', 9, 1),
+		new Letter('I', 9, 1),
+		new Letter('I', 9, 1),
+		new Letter('I', 9, 1),
+		new Letter('I', 9, 1),
+		new Letter('I', 9, 1),
+		new Letter('I', 9, 1),
+		new Letter('J', 1, 8),
+		new Letter('K', 1, 5),
+		new Letter('L', 4, 1),
+		new Letter('L', 4, 1),
+		new Letter('L', 4, 1),
+		new Letter('L', 4, 1),
+		new Letter('M', 2, 3),
+		new Letter('M', 2, 3),
+		new Letter('N', 6, 1),
+		new Letter('N', 6, 1),
+		new Letter('N', 6, 1),
+		new Letter('N', 6, 1),
+		new Letter('N', 6, 1),
+		new Letter('N', 6, 1),
+		new Letter('O', 8, 1),
+		new Letter('O', 8, 1),
+		new Letter('O', 8, 1),
+		new Letter('O', 8, 1),
+		new Letter('O', 8, 1),
+		new Letter('O', 8, 1),
+		new Letter('O', 8, 1),
+		new Letter('O', 8, 1),
+		new Letter('P', 2, 3),
+		new Letter('P', 2, 3),
+		new Letter('Q', 1, 10),
+		new Letter('R', 6, 1),
+		new Letter('R', 6, 1),
+		new Letter('R', 6, 1),
+		new Letter('R', 6, 1),
+		new Letter('R', 6, 1),
+		new Letter('R', 6, 1),
+		new Letter('S', 4, 1),
+		new Letter('S', 4, 1),
+		new Letter('S', 4, 1),
+		new Letter('S', 4, 1),
+		new Letter('T', 6, 1),
+		new Letter('T', 6, 1),
+		new Letter('T', 6, 1),
+		new Letter('T', 6, 1),
+		new Letter('T', 6, 1),
+		new Letter('T', 6, 1),
+		new Letter('U', 4, 1),
+		new Letter('U', 4, 1),
+		new Letter('U', 4, 1),
+		new Letter('U', 4, 1),
+		new Letter('V', 2, 4),
+		new Letter('V', 2, 4),
+		new Letter('W', 2, 4),
+		new Letter('W', 2, 4),
+		new Letter('X', 1, 8),
+		new Letter('Y', 2, 4),
+		new Letter('Y', 2, 4),
+		new Letter('Z', 1, 10),
+];
+
+var YOUR_HAND = new Array();
+var SCORE = 0;
+//开始游戏
+function startGame() {
+	addNumbersFromBag();
+	displayHand();
+	
+};
+
+
+//从词库中获取7个随机字母
+function addNumbersFromBag(){
+	console.log("your hand has:" + YOUR_HAND.length);
+	for(i=YOUR_HAND.length; i < 7; i++){
+		YOUR_HAND[i] = getAvailableLetter();
+	}
+	
+}
+
+
+function displayHand(){
+	console.log("your hand has:" + YOUR_HAND.length);
+	for (i = 0; i < YOUR_HAND.length; i++) {
+
+		console.log("#letter-" + (i+1) +" set to " + YOUR_HAND[i].letter);
+		$( "#letter-" + (i+1)).addClass("letter-" + YOUR_HAND[i].letter);
+		$( "#points-" + (i+1)).addClass("points-" + YOUR_HAND[i].pointsWhenLettersUsed);
+		
+		
+		
+		
+		$( "#letter-" + (i+1)).html(YOUR_HAND[i].letter);
+		
+		$( "#points-" + (i+1)).html(YOUR_HAND[i].pointsWhenLettersUsed);
+	}
+	
+}
+
+
+//获取可得字母
+function getAvailableLetter(){
+	var randomIndex = Math.floor(Math.random() * BAG_OF_LETTERS.length);//取任意字母
+	var randomLetter = BAG_OF_LETTERS.splice(randomIndex, 1);//从词库删除该字母
+	console.log(randomLetter[0]);
+	return randomLetter[0];//返回删除字母
+}
+
+
+function isfind(copy){
+	var myletter=new Array();
+	
+	var n=0;
+	
+	for(le=2;le<YOUR_HAND.length;le++){
+		for(i=0;i<le;i++){
+			myletter[i]=copy[i];
+			YOUR_HAND[i].used=true;
+		}
+		var strletter=myletter.join("");
+		if(isThisAWord(strletter)){
+			n=1;
+			break;
+		}
+		else{
+			resetHand();
+		}
+	}
+	if(n==1){
+		successfullyAddedWord(strletter);
+	}
+	else{
+			var t=copy[0];
+			copy[0]=copy[le];
+			copy[le]=t;
+			isfind(copy);
+	}
+}
+function findWordToUse(){
+ //TODO Your job starts here.
+	var copy=new Array();
+	for(i=0;i<YOUR_HAND.length;i++){
+		copy[i]=YOUR_HAND[i].letter;
+	}
+	isfind(copy);
+	if(BAG_OF_LETTERS.length==0)
+		return;
+}
+function humanFindWordToUse(){
+	
+	 var humanFoundWord = $( "#human-word-input").val();
+	 console.log("Checking human workd of:" + humanFoundWord);
+	 if(isThisAWord(humanFoundWord)){
+		 if(haveLettersForWord(humanFoundWord)){
+			 successfullyAddedWord(humanFoundWord);
+		 }else{
+			 alert(humanFoundWord + " - Do not have the letters for this word");
+		 }
+	 }else{
+		 alert(humanFoundWord + " is not a valid word.");
+	 }
+		
+}
+
+//成功找到单词记录
+function successfullyAddedWord(foundWord){
+	$( "#word-history-list").append("<li>" + foundWord + "</li>");
+	clearClasses();
+	takeOutUsedLetters();
+	addNumbersFromBag();
+	displayHand();
+	$( "#human-word-input").val('');
+	
+}
+//添加字母分数
+function addToScore(letterToAddToScore){
+	SCORE = SCORE + letterToAddToScore.pointsWhenLettersUsed;
+	console.log(letterToAddToScore.pointsWhenLettersUsed + "<-Points added for " + letterToAddToScore.letter);
+	$( "#score-number").html(SCORE);
+}
+
+//删除已使用字母并将分数添加
+function takeOutUsedLetters(){
+	
+	for(ii=0; ii < YOUR_HAND.length; ii++){
+		if(YOUR_HAND[ii].used){
+			addToScore(YOUR_HAND[ii]);
+			YOUR_HAND.splice(ii, 1);
+			ii = ii-1;
+		}else{
+			console.log(YOUR_HAND[ii].letter + "<- Not Used");
+		}
+	}
+	
+}
+//可以构成该单词
+function haveLettersForWord(aProposedWord){
+	//You could code the _ logic could go in this function
+	var wordAsArray = aProposedWord.toUpperCase().split("");//转换为大写，并分割为字符串数组
+	for (i = 0; i < wordAsArray.length; i++) {
+		var foundLetter = false;
+		console.log(wordAsArray[i] + "<-For match");
+		for(ii=0; ii<YOUR_HAND.length; ii++){
+			console.log("              " + YOUR_HAND[ii].letter + "<-Checking");
+			if(YOUR_HAND[ii].letter == wordAsArray [i]){
+				if(!YOUR_HAND[ii].used && !foundLetter){
+					console.log("     " + YOUR_HAND[ii].letter + "<-Found");
+					YOUR_HAND[ii].used = true;
+					foundLetter = true;
+					
+				}
+			}
+		}
+		
+		
+		if(!foundLetter){
+			resetHand();
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+//重置本次操作
+function resetHand(){
+	
+	for(ii=0; ii<YOUR_HAND.length; ii++){
+		YOUR_HAND[i].used = false;
+	}
+}
+//是否存在于单词列表中
+function isThisAWord(aProposedWord){
+	  if (Word_List.isInList(aProposedWord)) {
+	      return true;
+	  }
+	  return false;
+}
+//丢弃本次选择
+function retireHand(){
+	//Loose all the points in your hand
+	clearClasses();
+	YOUR_HAND = new Array();
+	addNumbersFromBag();
+	displayHand();
+}
+function clearClasses(){
+	for(ii=0; ii < YOUR_HAND.length; ii++){
+		$("#letter-" + (ii+1)).removeClass("letter-" + YOUR_HAND[ii].letter);
+		$("#points-" + (ii+1)).removeClass("points-" + YOUR_HAND[ii].pointsWhenLettersUsed);
+	}
+}
+
+$(document).ready(function() {
+	startGame();
+	
+	$("#find-word-button").click(function() {
+		findWordToUse();
+	});
+	$("#human-find-word-button").click(function() {
+		humanFindWordToUse();
+	});
+	$("#retire-hand-button").click(function() {
+		retireHand();
+	});
+	$('#human-word-input').keypress(function(event) {
+		if (event.which == 13) {
+			humanFindWordToUse();
+		}
+	});
+});
